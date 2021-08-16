855b1b50493d1c675a3f040095416026a8ee0753
Pierre FABIEN <p.fabien@caplaser.fr>
2021-08-16 17:51:34 +0200

    grained.js

--- js/index.js
+++ js/index.js
@@ -36,7 +36,20 @@ window.onload = function () {
 
 
 
-
+  var options = {
+    "animate": true,
+    "patternWidth": 100,
+    "patternHeight": 100,
+    "grainOpacity": 0.05,
+    "grainDensity": 1,
+    "grainWidth": 1,
+    "grainHeight": 1
+  }
+  grained("#stars", options);
+  
+  /*
+  background-color :rgb(204, 204, 204)
+  */
 
 
 
