6457af0f2eaa31d41aa5af0d5ac2db799c1b3478
Pierre FABIEN <p.fabien@caplaser.fr>
2021-08-18 17:58:24 +0200

    18.08

--- js/grained.js
+++ js/grained.js
@@ -1,10 +1,10 @@
 var options = {
   "animate": true,
-  "patternWidth": 250,
-  "patternHeight": 250,
-  "grainOpacity": 0.2,
+  "patternWidth": 500,
+  "patternHeight": 500,
+  "grainOpacity": 0.46,
   "grainDensity": 1,
   "grainWidth": 1,
   "grainHeight": 1
-  }
-  grained("#grained", options);
+}
+grained("#grained", options);
